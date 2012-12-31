00e05ba948b4a53dcece26938c476d793e950cf7
apoerio <apoerio@domanistudios.com>
2012-12-30 23:19:21 -0500

    First commit of basic files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '15f0cb35cac12aeb47d4487ffeeaabdd0457c90bfe4299ac70b1b2a8f7838232ed0dc17104bf2e4ae77a2624d2535edb83f3fc769b03361da6db581bbaf8efb8'
